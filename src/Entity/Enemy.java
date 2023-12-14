91bfde8991b59c19244be5001dc5d14df409adf9
Michał Banaszkiewicz <76440830+bnszky@users.noreply.github.com>
2023-12-14 11:40:00 +0100

    Changed radious to radius

--- src/Entity/Enemy.java
+++ src/Entity/Enemy.java
@@ -1,9 +1,9 @@
 package Entity;
 
 public class Enemy extends Mob{
-    public Enemy(int hp, byte radious, float angle, int[] position,
+    public Enemy(int hp, byte radius, float angle, int[] position,
                  int movementSpeed, boolean canShoot, int attackFrequency, int damage) {
-        super(hp, radious, angle, position, movementSpeed, canShoot, attackFrequency, damage);
+        super(hp, radius, angle, position, movementSpeed, canShoot, attackFrequency, damage);
     }
     public void Die(){
         //przyslonieta metoda dla klasy enemy -> usuniecie instancji tej klasy
