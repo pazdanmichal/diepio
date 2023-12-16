e430885e7c9c07fbdad9a795d81dbb560e442f31
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2023-12-16 23:57:27 +0100

    Dodanie Interfejsu Algorithm dla pozniejszych strategii sterowania playerem

---
+++ src/Algorithms/Algorithm.java
@@ -0,0 +1,10 @@
+package Algorithms;
+import Entity.Player;
+import Entity.Enemy;
+import java.util.ArrayList;
+public interface Algorithm {
+    public void Init(ArrayList<Enemy> enemies, Player player, long startTime); // Get data
+    public void Run(); // Preprocessing
+    public byte Move(long _time); // {-1 -> left, 0 -> stop, 1 -> right}
+    public boolean TryShoot(long _time); // true -> shoot, false -> no shoot
+}
