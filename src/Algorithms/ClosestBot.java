868aaf71fded16f9119851b2fef5503b767428ef
mleczako <marcinmmleczak@gmail.com>
2023-12-31 16:50:43 +0100

    fixed ClosestBot

--- src/Algorithms/ClosestBot.java
+++ src/Algorithms/ClosestBot.java
@@ -1,61 +1,67 @@
 package Algorithms;
 
 import BoardController.Screen;
-import Entity.Enemy;
-import Entity.Entity;
-import Entity.Player;
+import Entity.*;
 
 import java.util.List;
 
-public class ClosestBot implements Algorithm{
-    private long startTime;
+import static Algorithms.FastestBot.mod360;
+
+public class ClosestBot implements Algorithm {
     private Player player;
+    private List<Enemy> enemies;
+    private Enemy closestEnemy;
+
     @Override
-    public void Init(List<Enemy> enemies, Player player, long startTime) {
-        this.startTime = startTime;
-        this.player=player;
-    }
-    private long GetTimeFromStart(long _time){
-        return _time - startTime;
-    }
-    private float AngleDiff(float angle){
-        if (player.getCurrentRotation()-(angle-180) < 180 && player.getCurrentRotation()-(angle-180) < -180){
-            return player.getCurrentRotation()-(angle-180);
-        }else if(player.getCurrentRotation()-(angle-180)>180){
-            return (player.getCurrentRotation()-(angle-180))-360;
-        }else {
-            return 360+(player.getCurrentRotation()-(angle-180));
-        }
+    public void Init(Screen screen) {
+        this.player = Screen.getCurrentPlayer();
+        this.enemies = screen.enemyList();
+        closestEnemy = findClosest();
     }
 
-    private Enemy findClosest(){
-        float minAngle=360;
-        Enemy closestEnemy = null;
-        for(int i = 0; i<Screen.currentEntityTable.size(); i++){
-            if(Screen.currentEntityTable.get(i) instanceof Enemy){
-                if(AngleDiff(Screen.currentEntityTable.get(i).getAngle())<minAngle) {
-                    minAngle=AngleDiff(Screen.currentEntityTable.get(i).getAngle());
-                    closestEnemy=(Enemy) Screen.currentEntityTable.get(i);
-                }
+    protected float angleBetween(Entity entity) {
+        return Math.min(Math.abs(mod360(player.getAngle()) - mod360(entity.getAngle() + 180)), 360 - Math.abs(mod360(player.getAngle()) - mod360(entity.getAngle() + 180)));
+    }
+
+    private Enemy findClosest() {
+        if (enemies.isEmpty()) {
+            return null;
+        }
+        float minAngle = Float.MAX_VALUE;
+        for (Enemy enemy : enemies) {
+            if (angleBetween(enemy) < minAngle) {
+                minAngle = angleBetween(enemy);
+                closestEnemy = enemy;
             }
+
         }
         return closestEnemy;
     }
+
     @Override
-    public byte Move(long _time) {
-        if ((AngleDiff(findClosest().getAngle())<3 && AngleDiff(findClosest().getAngle())>-3)) return 0;
-        else if(AngleDiff(findClosest().getAngle())<0) return -1;
-        else return 1;
+    public byte Move() {
+        if (closestEnemy == null || angleBetween(closestEnemy) < 0.1) {
+            return 0;
+        } else if (mod360(mod360(player.getAngle()) - mod360(closestEnemy.getAngle() + 180)) >= 180) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 
     @Override
-    public boolean TryShoot(long _time) {
-        if (AngleDiff(findClosest().getAngle())<5 && AngleDiff(findClosest().getAngle())>-5) return true;
+    public boolean TryShoot() {
+        if (closestEnemy != null) {
+            for (Enemy enemy : enemies) {
+                if (angleBetween(enemy) < 1) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
-    public void FetchCurrentFrameInfo(long _time, boolean isShooted, List<Enemy> enemies, Player player) {
-        if(isShooted) { System.out.println("Czas: " + (double)GetTimeFromStart(_time)/1000 + ", Strzelono!"); }
+    public void FetchCurrentFrameInfo(boolean isShooted) {
     }
 }
