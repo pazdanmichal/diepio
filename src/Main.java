d077d971d017f2bc4a822f8a04a57ec81e0ab929
Maciek <maciej.durkalec@gmail.com>
2024-01-13 21:11:32 +0100

    poprawiony wave

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,7 @@ public class Main {
         Player player = new Player(
                 10, 50.0f, 270.0f, new float[]{0.0f, 0.0f},
                 0, true, 300, 1, (byte) 0,
-                1.5f, 1.7f, 0.65f, 7, 10, 0,2,40,1);
+                1.5f, 1.7f, 0.65f, 7, 10, 0,2,20,1);
 
         Menu launcher = new Menu(
                 playerAlgorithm -> {
